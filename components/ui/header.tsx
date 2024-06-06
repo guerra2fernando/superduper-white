7d637d50440e9c27d3ef5a408def09b5e016c83e
Fernando Guerra <smiklt@gmail.com>
2024-06-06 17:27:24 -0300

    submenu

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -19,19 +19,20 @@ export default function Header() {
             <ul className="flex grow justify-center flex-wrap items-center">
               <li className="relative group">
                 <Link
-                  className="font-medium text-sm text-slate-600 hover:text-slate-800 mx-4 lg:mx-5 transition duration-150 ease-in-out"
+                  className="font-medium text-sm text-slate-600 hover:text-slate-800 px-4 py-2 mx-4 lg:mx-5 transition duration-150 ease-in-out"
                   href="#"
                 >
                   Solutions
                 </Link>
-                <ul className="absolute top-full left-0 bg-slate-900 rounded shadow-lg pt-2 opacity-0 group-hover:opacity-100 group-hover:visible invisible transition-opacity duration-300 ease-in-out">
-                  <li>
+                <ul className="absolute left-1/2 transform -translate-x-1/2 mt-1 bg-slate-900 rounded shadow-lg min-w-[140px] opacity-0 group-hover:opacity-100 group-hover:visible invisible transition-opacity duration-300 ease-in-out">
+                  <div className="absolute left-1/2 transform mt-1 -translate-x-1/2 -top-2 w-4 h-4 bg-slate-900 rotate-45"></div>
+                  <li className="border-b border-zinc-700 last:border-none text-center">
                     <Link className="block px-4 py-2 text-sm text-slate-300 hover:text-white transition duration-150 ease-in-out" href="/solutions/key-value">Key Value</Link>
                   </li>
-                  <li>
+                  <li className="border-b border-zinc-700 last:border-none text-center">
                     <Link className="block px-4 py-2 text-sm text-slate-300 hover:text-white transition duration-150 ease-in-out" href="/solutions/vector-search">Vector Search</Link>
                   </li>
-                  <li>
+                  <li className="text-center">
                     <Link className="block px-4 py-2 text-sm text-slate-300 hover:text-white transition duration-150 ease-in-out" href="/solutions/in-database-rag">In-Database RAG</Link>
                   </li>
                 </ul>
@@ -42,7 +43,6 @@ export default function Header() {
               <li>
                 <Link className="font-medium text-sm text-slate-600 hover:text-slate-800 mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/pricing">Pricing</Link>
               </li>
-              
               <li>
                 <Link className="font-medium text-sm text-slate-600 hover:text-slate-800 mx-4 lg:mx-5 transition duration-150 ease-in-out" href="https://blog.superduperdb.com/">Blog</Link>
               </li>
@@ -56,10 +56,9 @@ export default function Header() {
           {/* Desktop sign in links */}
           <ul className="flex-1 flex justify-end items-center">
             <li className="ml-6">
-            <Link className="btn text-zinc-100 bg-zinc-900 hover:bg-zinc-800 w-full shadow" href="/enterprise">Get Started</Link>
+              <Link className="btn text-zinc-100 bg-zinc-900 hover:bg-zinc-800 w-full shadow" href="/enterprise">Get Started</Link>
             </li>
           </ul>
-
 
         </div>
       </div>
