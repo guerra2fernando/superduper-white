407080ddbfa40d8a6039b28e4eb6bfb6c187b7f5
Fernando Guerra <smiklt@gmail.com>
2024-06-18 18:48:32 +0300

    adjust integrations

--- components/integrations.tsx
+++ components/integrations.tsx
@@ -9,12 +9,21 @@ import cohere from '@/public/images/integrations/cohere.png';
 import langchain from '@/public/images/integrations/langchain.png';
 import lancedb from '@/public/images/integrations/lancedb.png';
 import llamaindex from '@/public/images/integrations/llamaindex.png';
-import qdrant from '@/public/images/integrations/qdrant.png';
-import pinecone from '@/public/images/integrations/pinecone.png';
 import scikitLearn from '@/public/images/integrations/scikit-learn.png';
-import weaviate from '@/public/images/integrations/weaviate.png';
 import pytorch from '@/public/images/integrations/pytorch.png';
-import postgresml from '@/public/images/integrations/postgresml.png';
+import amazonBedrock from '@/public/images/integrations/amazon-bedrock.png';
+import jina from '@/public/images/integrations/jina.png';
+import keras from '@/public/images/integrations/keras.png';
+import mistral from '@/public/images/integrations/mistral.png';
+import numpy from '@/public/images/integrations/numpy.png';
+import ollama from '@/public/images/integrations/ollama.png';
+import opencv from '@/public/images/integrations/opencv.png';
+import ray from '@/public/images/integrations/ray.png';
+import sentenceTransformers from '@/public/images/integrations/sentence-transformers.png';
+import spacy from '@/public/images/integrations/spacy.png';
+import stability from '@/public/images/integrations/stability.png';
+import unstructured from '@/public/images/integrations/unstructured.png';
+import vllm from '@/public/images/integrations/vllm.png';
 
 const integrations = [
     { src: anthropic, name: 'Anthropic' },
@@ -25,12 +34,22 @@ const integrations = [
     { src: langchain, name: 'LangChain' },
     { src: lancedb, name: 'LanceDB' },
     { src: llamaindex, name: 'LlamaIndex' },
-    { src: qdrant, name: 'Qdrant' },
-    { src: pinecone, name: 'Pinecone' },
     { src: scikitLearn, name: 'Scikit-Learn' },
-    { src: weaviate, name: 'Weaviate' },
     { src: pytorch, name: 'PyTorch' },
-    { src: postgresml, name: 'PostgresML' },
+    { src: amazonBedrock, name: 'Bedrock' },
+    { src: jina, name: 'Jina' },
+    { src: keras, name: 'Keras' },
+    { src: mistral, name: 'Mistral' },
+    { src: numpy, name: 'Numpy' },
+    { src: ollama, name: 'Ollama' },
+    { src: opencv, name: 'OpenCV' },
+    { src: ray, name: 'Ray' },
+    { src: sentenceTransformers, name: 'SBERT' },
+    { src: spacy, name: 'spaCy' },
+    { src: stability, name: 'Stability' },
+    { src: unstructured, name: 'Unstructured' },
+    { src: vllm, name: 'Vllm' },
+    // duplicate the list to create the scrolling effect
     { src: anthropic, name: 'Anthropic' },
     { src: huggingface, name: 'Hugging Face' },
     { src: openai, name: 'OpenAI' },
@@ -39,26 +58,21 @@ const integrations = [
     { src: langchain, name: 'LangChain' },
     { src: lancedb, name: 'LanceDB' },
     { src: llamaindex, name: 'LlamaIndex' },
-    { src: qdrant, name: 'Qdrant' },
-    { src: pinecone, name: 'Pinecone' },
     { src: scikitLearn, name: 'Scikit-Learn' },
-    { src: weaviate, name: 'Weaviate' },
     { src: pytorch, name: 'PyTorch' },
-    { src: postgresml, name: 'PostgresML' },
-    { src: anthropic, name: 'Anthropic' },
-    { src: huggingface, name: 'Hugging Face' },
-    { src: openai, name: 'OpenAI' },
-    { src: googleBigQuery, name: 'BigQuery' },
-    { src: cohere, name: 'Cohere' },
-    { src: langchain, name: 'LangChain' },
-    { src: lancedb, name: 'LanceDB' },
-    { src: llamaindex, name: 'LlamaIndex' },
-    { src: qdrant, name: 'Qdrant' },
-    { src: pinecone, name: 'Pinecone' },
-    { src: scikitLearn, name: 'Scikit-Learn' },
-    { src: weaviate, name: 'Weaviate' },
-    { src: pytorch, name: 'PyTorch' },
-    { src: postgresml, name: 'PostgresML' },
+    { src: amazonBedrock, name: 'Bedrock' },
+    { src: jina, name: 'Jina' },
+    { src: keras, name: 'Keras' },
+    { src: mistral, name: 'Mistral' },
+    { src: numpy, name: 'Numpy' },
+    { src: ollama, name: 'Ollama' },
+    { src: opencv, name: 'OpenCV' },
+    { src: ray, name: 'Ray' },
+    { src: sentenceTransformers, name: 'SBERT' },
+    { src: spacy, name: 'spaCy' },
+    { src: stability, name: 'Stability' },
+    { src: unstructured, name: 'Unstructured' },
+    { src: vllm, name: 'Vllm' }
 ];
 
 const IntegrationsSec: React.FC = () => {
