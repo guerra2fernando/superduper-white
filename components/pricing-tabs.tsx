aa3d22989984af25dc201b6e8db4020c86e14976
Fernando Guerra <smiklt@gmail.com>
2024-06-18 19:56:23 +0300

    pricing update

--- components/pricing-tabs.tsx
+++ components/pricing-tabs.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import Image from 'next/image'
+import { CheckIcon } from '@heroicons/react/24/solid'
 import PricingDecoration from '@/public/images/pricing-decoration.png'
 
 export default function PricingTabs() {
@@ -20,7 +21,7 @@ export default function PricingTabs() {
         "Basic AI workflow templates included",
         "Use with local or cloud environments"
       ],
-      link: "https://github.com/SuperduperDB/superduperdb"
+      link: "https://github.com/SuperDuperDB/superduperdb"
     },
     {
       title: "Enterprise / Custom",
@@ -46,7 +47,7 @@ export default function PricingTabs() {
   
   return (
     <section>
-      <div className="py-1 md:py-2">
+      <div className="py-12 md:py-20">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="pb-12 md:pb-20">
             <div className="flex justify-center items-center">
@@ -64,12 +65,10 @@ export default function PricingTabs() {
                       </div>
                       <div className="grow">
                         <div className={`text-sm ${index === 1 ? 'text-zinc-200' : 'text-zinc-900'} font-medium mb-4`}>Includes:</div>
-                        <ul className={`text-${index === 1 ? 'zinc-600 dark:text-zinc-400' : 'zinc-600 dark:text-zinc-400'} text-sm space-y-3 grow`}>
+                        <ul className={`text-${index === 1 ? 'zinc-400' : 'zinc-600'} text-sm space-y-3 grow`}>
                           {plan.features.map((feature, i) => (
-                            <li key={i} className="flex items-center">
-                              <svg className="w-3 h-3 fill-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                              </svg>
+                            <li key={i} className={`flex items-center p-2 ${index === 1 ? (i % 2 === 0 ? 'bg-[rgba(34,34,34,1)]' : 'bg-zinc-900') : (i % 2 === 0 ? 'bg-zinc-100' : 'bg-[rgba(232,232,232,1)]')} rounded-md`}>
+                              <CheckIcon className="w-5 h-5 text-emerald-500 mr-3" />
                               {feature}
                             </li>
                           ))}
